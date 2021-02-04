bcf7a643d0cba046194d9f2181eadda53ee86d7e
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-04 13:40:47 -1000

    sum all numbers in a range

--- intermediate.js
+++ intermediate.js
@@ -1,1 +1,12 @@
 // Sum all numbers in a range
+function sumAll(arr) {
+  var sum = 0;
+  for (var i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+sumAll([1, 4]);
+
+//
