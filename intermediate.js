d74864a3579d91ad5113eeb4c469a340b9cf07d0
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-04 13:45:28 -1000

    diff two arrays

--- intermediate.js
+++ intermediate.js
@@ -9,4 +9,13 @@ function sumAll(arr) {
 
 sumAll([1, 4]);
 
-//
+// Diff two arrays
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// Seek and destroy
