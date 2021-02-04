5c024fcfbaaf0020d2a84f4744ff0008f6bdc907
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-04 13:51:28 -1000

    Seek and destroy

--- intermediate.js
+++ intermediate.js
@@ -19,3 +19,12 @@ function diffArray(arr1, arr2) {
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 // Seek and destroy
+function destroyer(arr) {
+  var args = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  });
+}
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+//
