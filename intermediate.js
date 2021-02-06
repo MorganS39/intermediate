3828512e911aba5fda07d2fc253c865c42e5af3b
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-05 16:07:54 -1000

    sum all primes

--- intermediate.js
+++ intermediate.js
@@ -183,3 +183,22 @@ function sumFibs(num) {
 sumFibs(4);
 
 // Sum all primes
+function sumPrimes(num) {
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0)
+        return false;
+    }
+    return true;
+  }
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
+
+sumPrimes(10);
+
+// smallest common multiple
