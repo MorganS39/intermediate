8b24125c9285d68a85f2eeff0320e37d0125c525
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-05 14:58:36 -1000

    Missing letters

--- intermediate.js
+++ intermediate.js
@@ -109,4 +109,17 @@ function pairElement(str) {
 }
 pairElement("GCG");
 
-//
+// Missing letters
+function fearNotLetter(str) {
+  for (var i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    if (code !== str.charCodeAt(0) + i) {
+      return String.fromCharCode(code - 1);
+    }
+  }
+  return undefined;
+}
+
+fearNotLetter("abce");
+
+// Sorted union
