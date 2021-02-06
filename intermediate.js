769c11ebf1848a54b91b1deaedafa52d71276f49
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-05 15:33:52 -1000

    Convert HTML entities

--- intermediate.js
+++ intermediate.js
@@ -139,3 +139,30 @@ function uniteUnique(arr) {
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 
 // Convert HTML entities
+function convertHTML(str) {
+  function html(char) {
+    switch (char) {
+      case "&":
+        return ("&amp;");
+        break;
+      case "<":
+        return ("&lt;");
+        break;
+      case ">":
+        return ("&gt;");
+        break;
+      case '"':
+        return ("&quot;");
+        break;
+      case "'":
+        return ("&apos;");
+        break;
+    }
+  }
+    str = str.replace( /[&<>"']/g, html);
+    return str;
+}
+
+convertHTML("Dolce & Gabbana");
+
+// Sum all odd fibonacci numbers
