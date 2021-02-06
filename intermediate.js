5995ed093ee322dfa0f71baee114c91c23ac2dc0
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-05 14:50:34 -1000

    DNA Pairing

--- intermediate.js
+++ intermediate.js
@@ -82,4 +82,31 @@ function myReplace(str, before, after) {
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
 
+// DNA pairing
+function pairElement(str) {
+  var pairs = [];
+  
+  var swap = function(char) {
+    switch (char) {
+      case "A":
+        pairs.push(["A", "T"]);
+        break;
+      case "T":
+        pairs.push(["T", "A"]);
+        break;
+      case "C":
+        pairs.push(["C", "G"]);
+        break;
+      case "G":
+        pairs.push(["G", "C"]);
+        break; 
+    }
+  };
+  for (var i = 0; i < str.length; i++) {
+    swap(str[i]);
+  }
+  return pairs;
+}
+pairElement("GCG");
+
 //
